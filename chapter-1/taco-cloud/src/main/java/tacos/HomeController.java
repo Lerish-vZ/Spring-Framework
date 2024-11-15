2aaae638e5f2503acb68d3d819b22f6625cb65ec
Lerish-vZ <lerishavz@gmail.com>
2024-11-15 11:28:19 +0200

    update

--- chapter-1/taco-cloud/src/main/java/tacos/HomeController.java
+++ chapter-1/taco-cloud/src/main/java/tacos/HomeController.java
@@ -1,10 +1,12 @@
 package tacos;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class HomeController {
-  @GetMapping("/")
-  public String home() {
-    return "home";
-  }
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
 }
