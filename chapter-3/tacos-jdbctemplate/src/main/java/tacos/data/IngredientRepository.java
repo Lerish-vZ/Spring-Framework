0e94f38fdd75a2d4426d0696fa6a0c0eb28627a5
Lerish-vZ <lerishavz@gmail.com>
2024-11-28 09:55:06 +0200

    update

--- chapter-3/tacos-jdbctemplate/src/main/java/tacos/data/IngredientRepository.java
+++ chapter-3/tacos-jdbctemplate/src/main/java/tacos/data/IngredientRepository.java
@@ -4,6 +4,11 @@ import java.util.Optional;
 
 import tacos.Ingredient;
 
+//Defines the following operations as method declarations:
+//    Query for all ingredients into a collection of Ingredient objects
+//    Query for a single Ingredient by its id
+//    Save an Ingredient object
+
 public interface IngredientRepository {
 
   Iterable<Ingredient> findAll();
