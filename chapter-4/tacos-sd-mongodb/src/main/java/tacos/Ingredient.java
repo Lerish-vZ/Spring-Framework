39908c13eb5b1155b17250817fc07fd062f5bf4b
Lerish-vZ <lerishavz@gmail.com>
2024-12-03 12:13:44 +0200

    update

--- chapter-4/tacos-sd-mongodb/src/main/java/tacos/Ingredient.java
+++ chapter-4/tacos-sd-mongodb/src/main/java/tacos/Ingredient.java
@@ -9,7 +9,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Document
+@Document //placed at the class level to indicate that Ingredient is a document entity that can be written to and read form a Mongo database
+//By default, the collection name is based on the class name, with the first letter lowercase. You can specify what you want the collection name to be by setting the collection attribute of @Document [@Document (collection="ingredients")]
 @AllArgsConstructor
 @NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
 public class Ingredient {
